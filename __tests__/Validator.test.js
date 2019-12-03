076ca7c3e61ce7b7e7bdaecac0a8a0044b8af956
Lisa Carpenter <carpenter.lisa@gmail.com>
2019-12-03 14:23:52 -0800

    wrote tests for validate method

--- __tests__/Validator.test.js
+++ __tests__/Validator.test.js
@@ -1,7 +1,34 @@
+const { Validator } = require('../lib/Validator');
+
+const dog = {
+  name: 'spot',
+  age: '5',
+  weight: '20 lbs'
+};
+
+const dog2 = {
+  age: two,
+  weight: 20
+};
+
+const dog3 = {
+  name: 3.14159,
+  age: 5,
+  weight: '15 lbs'
+};
+
+const nameValidator1 = new Validator('name', { type: String, required: true });
+const nameValidator2 = new Validator('name', { type: String, required: false });
+
 describe('validator module', () => {
-  describe('Validator class', () => {
-    it('validate method can take an object and return a fields value', () => {
-      expect(isNumber(3)).toBeTruthy();
+  describe('validate method', () => {
+    it('takes an object and returns a fields value', () => {
+      expect(nameValidator1.validate(dog)).toEqual('spot');
+      expect(nameValidator2.validate(dog)).toEqual('spot');
+      expect(nameValidator1.validate(dog2)).toThrowErrorMatchingSnapshot();
+      expect(nameValidator2.validate(dog2)).toThrowErrorMatchingSnapshot();
+      expect(nameValidator1.validate(dog3)).toEqual('3.14159');
+      expect(nameValidator2.validate(dog3)).toEqual('3.14159');
     });
 
     it('validate method can take an object and throw an error', () => {
--- index.js
+++ index.js
@@ -15,12 +15,12 @@ const colorValidator = new Validator('color', {
   required: true
 });
   
-  const dog = {
-    name: 'spot',
-    age: '5',
-    weight: '20 lbs'
-  };
+const dog = {
+  name: 'spot',
+  age: '5',
+  weight: '20 lbs'
+};
   
-  nameValidator.validate(dog) // returns 'spot'
-  age.validate(dog) // returns 5
-  colorValidator.validate(dog) // throws error
+nameValidator.validate(dog) // returns 'spot'
+ageValidator.validate(dog) // returns 5
+colorValidator.validate(dog) // throws error
